6c333a6384ec4ff754096015ba6318f169e3133f
Isaac Laquerre <killers3d@gmail.com>
2026-02-19 10:03:11 -0500

    Finished express solution

--- express/dist/public/assets/js/script.js
+++ express/dist/public/assets/js/script.js
@@ -1,3 +1,52 @@
+const placeholders = [
+  'Do my laundry',
+  'Walk the dog',
+  'Buy groceries',
+  'Make my bed',
+  'Read 30 minutes',
+  'Fold and put away clean clothes',
+  'Clean and organize my desk',
+  'Plan meals for the week',
+  'Empty trash',
+  'Take recycling out',
+  'Wash dishes',
+  'Sweep and mop kitchen floor',
+  'Change bed sheets',
+  'Review monthly budget',
+  'Organize desktop files',
+  'Refill windshield washer fluid',
+  'Prepare lunch for tomorrow',
+  'Clean litter box',
+  'Mow the lawn',
+  'Rake leaves',
+  'Shovel snow',
+  'Water plants'
+];
+
+let totalTasks = 0;
+let completedTasks = 0;
+
+function load() {
+  setPlaceholder();
+
+  setInterval(() => {
+    setPlaceholder();
+  }, 5_000)
+}
+
+function setPlaceholder() {
+  let oldPlaceholder = query('#newTask').children[2].placeholder;
+  let newPlaceholder = placeholders[Math.floor(Math.random() * placeholders.length)];
+  if (newPlaceholder === oldPlaceholder) return setPlaceholder();
+  query('#newTask').children[2].setAttribute('placeholder', newPlaceholder);
+}
+
+function updatePercentage() {
+  totalTasks = Array.from(document.querySelectorAll('span.task-title')).filter(el => el.id !== "newTask").length;
+  completedTasks = Array.from(document.querySelectorAll('li.task.checked')).filter(el => el.id !== "newTask").length;
+  query('#percentage').innerHTML = `${completedTasks}/${totalTasks} completed`
+}
+
 function updateTaskList() {
   let tasks = query('#tasks');
   if (!tasks) return;
@@ -7,12 +56,11 @@ function updateTaskList() {
   if (!lastChild || lastChild === undefined) return;
   let taskTitle = tasks.children[tasks.children.length - 1].children[2];
   if (!taskTitle || taskTitle === undefined);
-  if (taskTitle.value === '' || taskTitle.value.length === 0) return;
-  if (taskTitle.tagName === "INPUT") {
+  if (taskTitle.value === '' || taskTitle.value.length === 0) taskTitle.value = taskTitle.placeholder;
+  if (taskTitle.tagName === 'INPUT') {
     let newTask = newEl('li');
     newTask.classList.add('task');
     if (lastChild.children[0].checked) newTask.classList.add('checked');
-    newTask.setAttribute('onclick', 'check(this);');
     if (!lastChild.children[0].checked) tasks.insertBefore(newTask, lastChild);
     else query('#completedTasks').appendChild(newTask);
 
@@ -25,36 +73,57 @@ function updateTaskList() {
 
     let checkSpan = newEl('span');
     checkSpan.classList.add('checkmark');
+    checkSpan.setAttribute('onclick', 'check(this.parentNode);');
     newTask.appendChild(checkSpan);
 
     let newTaskTitle = newEl('span');
     newTaskTitle.classList.add('task-title');
     newTaskTitle.innerHTML = taskTitle.value;
     newTask.appendChild(newTaskTitle);
+
+    let newTaskDelete = newEl('span');
+    newTaskDelete.classList.add('task-delete');
+    newTaskDelete.setAttribute('onclick', 'deleteTask(this.parentNode);');
+    newTaskDelete.innerHTML = 'x';
+    newTask.appendChild(newTaskDelete);
+
+    taskTitle.value = '';
+    setPlaceholder();
+    totalTasks = document.querySelectorAll('span.task-title').length;
+    
+    updatePercentage();
   }
 }
 
 function check(task) {
-  for (let i = 0; i < task.children.length; i++) {
-    let child = task.children[0];
-    if (child === undefined || child.classList === undefined || child.checked === undefined) return;
-    if (child.classList.contains('task-checkbox')) {
-      task.classList.toggle('checked');
-      child.checked = !child.checked;
-      let textChild = task.children[2];
-      if (textChild === undefined || textChild.tagName === undefined) return;
-      if (textChild.tagName !== "INPUT") {
-        if (child.checked) {
-            move(task, query('#completedTasks'));
-            task.title = 'Unmark "' + task.children[2].innerHTML + '" as completed'
-          } else {
-            moveBefore(task, query("#tasks"), query('#newTask'));
-            task.title = 'Mark "' + task.children[2].innerHTML + '" as completed';
-          }
-      }
+  let child = task.children[0];
+  if (child === undefined || child.classList === undefined || child.checked === undefined) return;
+  if (child.classList.contains('task-checkbox')) {
+    task.classList.toggle('checked');
+    child.checked = !child.checked;
+    let textChild = task.children[2];
+    if (textChild === undefined || textChild.tagName === undefined) return;
+    if (textChild.tagName !== 'INPUT') {
+      if (child.checked) {
+          move(task, query('#completedTasks'));
+          task.title = 'Unmark "' + task.children[2].innerHTML + '" as completed'
+        } else {
+          moveBefore(task, query('#tasks'), query('#newTask'));
+          task.title = 'Mark "' + task.children[2].innerHTML + '" as completed';
+        }
     }
   }
+
+  updatePercentage();
 }
+
+function deleteTask(task) {
+  task.remove();
+
+  updatePercentage();
+}
+
+// Utils
 
 function query(selector) {
   return document.querySelector(selector);
