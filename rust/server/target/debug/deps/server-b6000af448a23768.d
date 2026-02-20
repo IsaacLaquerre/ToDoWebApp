e451c2bcdf39cf6929e61d5c543a3b0bc26c0887
Isaac Laquerre <60407229+IsaacLaquerre@users.noreply.github.com>
2026-02-20 11:13:10 -0500

    Delete rust/server/target directory

--- rust/server/target/debug/deps/server-b6000af448a23768.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ilaquerre\Documents\GitHub\ToDoWebApp\rust\server\target\debug\deps\server-b6000af448a23768.d: src\main.rs
-
-C:\Users\ilaquerre\Documents\GitHub\ToDoWebApp\rust\server\target\debug\deps\libserver-b6000af448a23768.rmeta: src\main.rs
-
-src\main.rs:
--- rust/server/target/debug/server.d
+++
@@ -1,1 +0,0 @@
-C:\Users\ilaquerre\Documents\GitHub\ToDoWebApp\rust\server\target\debug\server.exe: C:\Users\ilaquerre\Documents\GitHub\ToDoWebApp\rust\server\src\main.rs
